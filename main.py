2617266100499198ebb5048a87b42120c0333069
Kazandu <github@kazandu.moe>
2022-01-29 05:35:32 +0100

    test

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def say_hello():
 def checkerlist():
     with open("/opt/archiving/ytdlppython/check_batch.txt") as check_batch_file:
         check_batch= check_batch_file.readlines()
-        check_batch_result= check_batch.strip('\n')
+        check_batch_result= [line.strip() for line in check_batch]
         return jsonify(check_batch_result)
 
 

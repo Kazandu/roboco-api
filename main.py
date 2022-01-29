e47fa95a825e7da75aa5128bf3260bed1b1dcd8f
Kazandu <github@kazandu.moe>
2022-01-29 05:14:32 +0100

    init testing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +9,12 @@ def index():
 @app.route('/health')
 def say_hello():
   return 'Hello from Server'
+
+@app.route('/checker/list')
+def checkerlist():
+    with open("/opt/archiving/ytdlppython/check_batch.txt") as check_batch_file:
+        return check_batch_file.read()
+
+
+if __name__ == "__main__":
+    serve(app, host="127.0.0.1", port=5069)
